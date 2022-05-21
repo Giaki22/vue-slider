a2dba43baac8d32d4bb74dda4319765a42e4f9c8
Giaki22 <giaki22@hotmail.com>
2022-05-21 17:01:21 +0200

    + Working buttons

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
-const images = [
-    {
-        src: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+let app = new Vue( {
+    el: '#app',
+    data: {
+            images: [
+                {
+                    src: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    src: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum',
+                },
+                {
+                    src: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    src: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                },
+                {
+                    src: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                },
+            ],
+            index : 0,
     },
-    {
-        src: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum',
-    },
-    {
-        src: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        src: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-    {
-        src: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-];
+    methods: {
+        nextPic() {
+            this.index === this.images.length - 1 ? this.index = 0 : this.index++;
+        },
+        previousPic() {
+            this.index === 0 ? this.index = this.images.length - 1 : this.index--;
+        },
+    }
+});
